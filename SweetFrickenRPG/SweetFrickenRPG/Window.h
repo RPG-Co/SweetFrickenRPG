0a7bf66773a989bdddc4361382f456af49ff1644
ReidHymes <reidhymes@gmail.com>
2013-11-04 19:38:50 -0800

    First Commit

---
+++ SweetFrickenRPG/SweetFrickenRPG/Window.h
@@ -0,0 +1,65 @@
+////////////////////////////////////////////////////////////////////////////////
+// Filename: Window.h
+////////////////////////////////////////////////////////////////////////////////
+#ifndef WINDOW_H
+#define WINDOW_H
+
+///////////////////////////////
+// PRE-PROCESSING DIRECTIVES //
+///////////////////////////////
+#define WIN32_LEAN_AND_MEAN
+
+//////////////
+// INCLUDES //
+//////////////
+#include <windows.h>
+
+///////////////////////
+// MY CLASS INCLUDES //
+///////////////////////
+#include "Input.h"
+#include "Graphics.h"
+
+////////////////////////////////////////////////////////////////////////////////
+// Class name: Window
+////////////////////////////////////////////////////////////////////////////////
+class Window
+{
+public:
+	Window();
+	Window(const Window&);
+	~Window();
+
+	bool Initialize();
+	void Shutdown();
+	void Run();
+
+	LRESULT CALLBACK MessageHandler(HWND, UINT, WPARAM, LPARAM);
+
+private:
+	bool Frame();
+	void InitializeWindows(int&, int&);
+	void ShutdownWindows();
+
+private:
+	char * m_applicationName;
+	HINSTANCE m_hinstance;
+	HWND m_hwnd;
+
+	Input* m_Input;
+	Graphics* m_Graphics;
+};
+
+
+/////////////////////////
+// FUNCTION PROTOTYPES //
+/////////////////////////
+static LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+
+/////////////
+// GLOBALS //
+/////////////
+static Window* ApplicationHandle = 0;
+
+#endif
